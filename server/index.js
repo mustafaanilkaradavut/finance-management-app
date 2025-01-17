143b8fffef12f00c22ff1e435e0b86df7572a19b
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-01-17 06:47:01 +0000

    kpi

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import kpiRoutes from './routes/kpi.js';
 
 // ----------------------------- CONFIGURATIONS ----------------------------- */
 
@@ -18,12 +19,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+// --------------------------------- ROUTES --------------------------------- */
+app.use('/kpi,kpiRoutes');
+
 // ----------------------------- MONGOOSE SETUP ----------------------------- */
 
 const PORT = process.env.PORT || 9000;
 mongoose
    .connect(process.env.MONGO_URL)
    .then(async () => {
-      app.listen(PORT, () => console.log(`Server Port: ${PORT}`)); 
+      app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
    })
    .catch((error) => console.log(`${error} Did Not Connect`));
