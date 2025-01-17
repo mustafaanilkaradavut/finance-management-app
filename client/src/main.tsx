ec28f543a06c90d0eb6815143d769dce48c4a207
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-01-17 02:04:16 +0000

    end of layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-createRoot(document.getElementById('root')!).render(
-   <StrictMode>
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+   <React.StrictMode>
       <App />
-   </StrictMode>
+   </React.StrictMode>
 );
