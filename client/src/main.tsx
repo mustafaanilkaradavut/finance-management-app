a3cec5938d026d22d2697a8083a2299057ac881d
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-01-17 02:09:11 +0000

    redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from './state/api';
 
+export const store = configureStore({
+   reducer: { [api.reducerPath]: api.reducer },
+   middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-   <React.StrictMode>
+   <Provider store={store}>
       <App />
-   </React.StrictMode>
+   </Provider>
 );
