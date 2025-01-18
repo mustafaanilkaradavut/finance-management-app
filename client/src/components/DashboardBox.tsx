248fa2a85067ed61a87e02857c669d25bb8faea8
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-01-18 14:27:27 +0000

    fixed

--- client/src/components/DashboardBox.tsx
+++ client/src/components/DashboardBox.tsx
@@ -5,6 +5,14 @@ const DashboardBox = styled(Box)(({ theme }) => ({
    backgroundColor: theme.palette.background.light,
    borderRadius: '1rem',
    boxShadow: '0.15rem 0.2rem 0.15rem 0.1rem rgba(0, 0, 0, .8)',
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-between',
+   alignItems: 'center',
+   minHeight: '250px', 
+   height: '100%',
+   flexGrow: 1,
+   padding: '1rem',
 }));
 
 export default DashboardBox;
