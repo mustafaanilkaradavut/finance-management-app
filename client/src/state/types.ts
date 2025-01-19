37a1faf55317cae6b582a868007ad18ef479ee13
Mustafa Anil Karadavut <mustafaanilkaradavut@gmail.com>
2025-01-19 10:26:01 +0000

    define_terms

--- client/src/state/types.ts
+++ client/src/state/types.ts
@@ -18,8 +18,6 @@ export interface Day {
    date: string;
    revenue: number;
    expenses: number;
-   nonOperationalExpenses: number;
-   operationalExpenses: number;
 }
 
 export interface GetKpisResponse {
@@ -32,6 +30,28 @@ export interface GetKpisResponse {
    expensesByCategory: ExpensesByCategory;
    monthlyData: Array<Month>;
    dailyData: Array<Day>;
+   createdAt: string;
+   updatedAt: string;
+}
+
+export interface GetProductsResponse {
+   id: string;
+   _id: string;
+   __v: number;
+   price: number;
+   expense: number;
+   transactions: Array<string>;
+   createdAt: string;
+   updatedAt: string;
+}
+
+export interface GetTransactionsResponse {
+   id: string;
+   _id: string;
+   __v: number;
+   buyer: string;
+   amount: number;
+   productIds: Array<string>;
    createdAt: string;
    updatedAt: string;
 }
